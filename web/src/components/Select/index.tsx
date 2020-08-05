7d072c6501b21edb50cffcc5139a41f30991c4c3
ruifernandees <dev.ruifernandes@gmail.com>
2020-08-05 11:13:22 -0300

    TeacherForm finished and integrated with the API

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -18,8 +18,9 @@ const Select: React.FC<SelectProps> = (props) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select id={name} {...rest} defaultValue="optionSelect">
-        <option value="optionSelect" disabled hidden>Selecione uma opção</option>
+      <select value="" id={name} {...rest} >
+        <option value="" disabled hidden>Selecione uma opção</option>
+
         {options.map(option => {
           return <option key={option.value} value={option.value}>{option.label}</option>
         })}
