584d73f59ad74191e75a7947fe43cb2b7ca247ec
ruifernandees <dev.ruifernandes@gmail.com>
2020-08-06 18:19:52 -0300

    Mobile App: Tabs navigation setup

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -18,6 +18,10 @@ const Landing = () => {
     navigate("GiveClasses");
   }
 
+  function handleNavigateToStudyPages() {
+    navigate("Study");
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -28,7 +32,10 @@ const Landing = () => {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton 
+          style={[styles.button, styles.buttonPrimary]}
+          onPress={handleNavigateToStudyPages}
+        >
           <Image source={studyIcon}  />
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
